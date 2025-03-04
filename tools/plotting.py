18fea3e975348fb81139605729fe5c4a616d1351
Noa Bengtsson <noab@chalmers.se>
2025-03-04 18:04:19 +0100

    Plotting

--- tools/plotting.py
+++ tools/plotting.py
@@ -67,6 +67,7 @@ def openmp_threashold(df: pandas.DataFrame):
 def speedup_benchmark_plots(df: pandas.DataFrame):
     nr_nodes = df["mpi_nodes"].unique()
     fig, ax = plt.subplots()
+    global baseline
     baseline = df.query("mpi_nodes == 1 and omp_threads == 1")["runtime"].max()
     for node in nr_nodes:
         sub_df = df.query("mpi_nodes == @node")
@@ -100,7 +101,37 @@ def speedup_benchmark_plots(df: pandas.DataFrame):
     
 
 
+def speedup_omp_levels(df: pandas.DataFrame):
+    fig, ax = plt.subplots()
+    global baseline
+    print(baseline)
+    ordering = df["runtime"].sort_values().index
+    ax.plot(df["omp_max_active_levels"][ordering], baseline/df["runtime"][ordering])
+    ax.set_ylabel("Speedup", fontsize=16)
+    ax.set_xlabel("OpenMP nested levels", fontsize=16)
+    ax.set_title("$N=10^6$", fontsize=16)
+    ax.xaxis.set_major_locator(plticker.IndexLocator(1,0))
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.tick_params(axis='both', which='minor', labelsize=8)
+    fig.savefig("./plots/omp_levels_benchmark.png", dpi=300)
 
+
+def mpi_speedup(df: pandas.DataFrame):
+    fig, ax = plt.subplots()
+    global baseline
+    sub_df = df.query("omp_threads==1")
+
+    ordering = sub_df["mpi_nodes"].sort_values().index
+    ax.plot(sub_df["mpi_nodes"][ordering], baseline/sub_df["runtime"][ordering])
+    
+    ax.set_ylabel("Speedup", fontsize=16)
+    ax.set_xlabel("MPI nodes", fontsize=16)
+    ax.set_title("$N=10^6$", fontsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.tick_params(axis='both', which='minor', labelsize=8)
+    ax.xaxis.set_major_locator(plticker.IndexLocator(1,0))
+    #fig.legend(loc=7, bbox_to_anchor=(1,0.3))
+    fig.savefig(f"./plots/mpi_1thread.png", dpi=300)
 
 
 
@@ -121,5 +152,11 @@ mpi_depth_optimum_depth_and_exchanged_bodies(df_mpi_depth, 2)
 runtime_bench = pandas.read_csv(r"../benchmarks/vera-1-mpi-runtime-no-ompnest",header=0, decimal=".", delimiter="\t")
 
 speedup_benchmark_plots(runtime_bench)
+
+#levels_bench = pandas.read_csv(r"../levelsTest/treecodeBenchmark.txt",header=0, decimal=".", delimiter="\t")
+
+#speedup_omp_levels(levels_bench)
+
+mpi_speedup(runtime_bench)
 
 
