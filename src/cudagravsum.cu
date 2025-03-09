623e778f1d85c2ba57ccca79e0b433153458bbed
xinglug <xinglug@chalmers.se>
2025-03-10 01:33:33 +0800

    Try coalesced memory access

--- src/cudagravsum.cu
+++ src/cudagravsum.cu
@@ -26,8 +26,10 @@ typedef struct {
 
 #ifndef DOUBLEPREC
 #define cuda_vector MAKE_TYPE(float, NDIM)
+#define pos_mass_vector MAKE_TYPE(float, PMDIM)
 #else
 #define cuda_vector MAKE_TYPE(double, NDIM)
+#define pos_mass_vector MAKE_TYPE(double, PMDIM)
 #endif
 
 
@@ -35,10 +37,12 @@ typedef struct {
 #define CUDA_VECTOR_TO_VECTOR(cv) ((vector){(cv).x, (cv).y})
 #define VECTOR_TO_CUDA_VECTOR(v) ((cuda_vector){(v)[0], (v)[1]})
 #define CLR_CUDA_VECTOR(cv) ((cv) = {0, 0})
+#define PMDIM 3
 #elif NDIM == 3
 #define CUDA_VECTOR_TO_VECTOR(cv) ((vector){(cv).x, (cv).y, (cv).z})
 #define VECTOR_TO_CUDA_VECTOR(v) ((cuda_vector){(v)[0], (v)[1], (v)[2]})
 #define CLR_CUDA_VECTOR(cv) ((cv) = {0, 0, 0})
+#define PMDIM 4
 #elif NDIM == 4
 #define CUDA_VECTOR_TO_VECTOR(cv) ((vector){(cv).x, (cv).y, (cv).z, (cv).w})
 #define VECTOR_TO_CUDA_VECTOR(v) ((cuda_vector){(v)[0], (v)[1], (v)[2], (v)[3]})
@@ -62,6 +66,8 @@ static std::vector<uint32_t> h_interact_vecs;
 static std::vector<uint32_t> h_offset;
 
 static std::vector<size_t> h_bodies_to_process;
+
+static std::vector<pos_mass_vector> h_pos_mass_vecs;
 
 real* d_out_phi_raw;
 
@@ -116,6 +122,60 @@ __global__ void cuda_node_calc_kernel(real eps2, uint32_t* interact_lists, uint3
     out_phi_list[current_body_index] = local_phi0;
     out_acc_list[current_body_index] = local_acc0;
 }
+
+
+__global__ void cuda_node_calc_coalescing_kernel(real eps2, pos_mass_vector* pos_mass_lists, uint32_t* offset, size_t* bodies_to_process,
+    real *out_phi_list, cuda_vector* out_acc_list, size_t n_bodies){
+
+    uint32_t list_index = blockIdx.x*blockDim.x + threadIdx.x; 
+
+    if (list_index >= n_bodies)
+        return;
+    uint32_t end = offset[list_index];
+    uint32_t start;
+    if (list_index == 0){
+        start = 1;
+    }
+    else{
+        start = offset[list_index-1]+1;
+    }
+
+    real dr2, drab, phi_p, mr3i;
+    //vector dr;
+    cuda_vector dr;
+    real local_phi0 = 0;
+    cuda_vector local_acc0;
+    //CLRV(local_acc0);
+    CLR_CUDA_VECTOR(local_acc0);
+    
+    size_t current_body_index = bodies_to_process[list_index];
+    pos_mass_vector current_body_pos_mass = pos_mass_lists[start-1];
+    //real current_body_mass = current_body_pos_mass.w;
+    cuda_vector current_body_pos = make_float3(current_body_pos_mass.x, current_body_pos_mass.y, current_body_pos_mass.z);
+
+    for (uint32_t i = start; i < end; i++)
+    {   
+        //size_t loop_body_index = interact_lists[i];
+        pos_mass_vector loop_body_pos_mass = pos_mass_lists[i];
+        real loop_body_mass = loop_body_pos_mass.w;
+        cuda_vector loop_body_pos = make_float3(loop_body_pos_mass.x, loop_body_pos_mass.y, loop_body_pos_mass.z);
+
+        dr = loop_body_pos - current_body_pos;
+        dr2 = dot(dr, dr);
+        dr2 += eps2;
+        drab = sqrt(dr2);
+        phi_p = loop_body_mass/drab;
+        local_phi0 -= phi_p;
+        mr3i = phi_p/dr2;
+        //ADDMULVS(local_acc0, dr, mr3i);
+        local_acc0 += dr * mr3i;
+    }
+
+    out_phi_list[current_body_index] = local_phi0;
+    out_acc_list[current_body_index] = local_acc0;
+
+}
+
 
 __global__ void cuda_node_reduction_kernel(real eps2, uint32_t* interact_lists, uint32_t* offset, size_t* bodies_to_process,
     cuda_vector* pos_list, real* mass_list, real *out_phi_list, cuda_vector* out_acc_list, size_t n_bodies){
@@ -184,24 +244,32 @@ __global__ void cuda_node_reduction_kernel(real eps2, uint32_t* interact_lists, 
 
 void cuda_gravsum(bodyptr current_body, cell_ll_entry_t *cell_list_tail, cell_ll_entry_t *body_list_tail) {
 
+    h_pos_mass_vecs.push_back(make_float4(VECTOR_TO_CUDA_VECTOR(Pos(current_body)), Mass(current_body)));
+        
+
     cell_ll_entry_t *curr_list_entry = cell_list_tail;
     while (curr_list_entry->priv != nullptr) {       
-        h_interact_vecs.push_back(curr_list_entry->index + nbody);;
+        //h_interact_vecs.push_back(curr_list_entry->index + nbody);
+        cellptr loop_cell_index = celltab + curr_list_entry->index;
+        h_pos_mass_vecs.push_back(make_float4(VECTOR_TO_CUDA_VECTOR(Pos(loop_cell_index)), Mass(loop_cell_index)));
         curr_list_entry = curr_list_entry->priv;
     }
 
     curr_list_entry = body_list_tail;
     while (curr_list_entry->priv != nullptr) {     
-        h_interact_vecs.push_back(curr_list_entry->index);
+        //h_interact_vecs.push_back(curr_list_entry->index);
+        bodyptr loop_body_index = bodytab + curr_list_entry->index;
+        h_pos_mass_vecs.push_back(make_float4(VECTOR_TO_CUDA_VECTOR(Pos(loop_body_index)), Mass(loop_body_index)));
         curr_list_entry = curr_list_entry->priv;
     } 
 
-    h_offset.push_back(h_interact_vecs.size()); // = end
+    h_offset.push_back(h_pos_mass_vecs.size()); // = end
     h_bodies_to_process.push_back((size_t)(current_body-bodytab));
 
-    if (h_bodies_to_process.size()==BLOCK_SIZE*256){
+    if (h_bodies_to_process.size()==BLOCK_SIZE*16){
         cuda_gravsum_dispatch();
-        h_interact_vecs.clear();
+        //h_interact_vecs.clear();
+        h_pos_mass_vecs.clear();
         h_bodies_to_process.clear();
         h_offset.clear();
     }
@@ -216,36 +284,22 @@ void cuda_gravsum_dispatch()
 
     size_t nBodiesToProcess = h_bodies_to_process.size();
 
-    uint32_t* h_interact_vecs_raw = h_interact_vecs.data();
+    //uint32_t* h_interact_vecs_raw = h_interact_vecs.data();
+    pos_mass_vector* h_pos_mass_vecs_raw = h_pos_mass_vecs.data();
     uint32_t* h_offset_raw = h_offset.data();
     size_t* h_bodies_to_process_raw = h_bodies_to_process.data();
     // real h_out_phi_raw[nBodiesToProcess];
     // cuda_vector h_out_acc_raw[nBodiesToProcess]; 
 
 
-    uint32_t* d_interact_vecs_raw;
-    cudaMalloc(&d_interact_vecs_raw, sizeof(uint32_t)*h_interact_vecs.size());
+    // uint32_t* d_interact_vecs_raw;
+    // cudaMalloc(&d_interact_vecs_raw, sizeof(uint32_t)*h_interact_vecs.size());
+    pos_mass_vector* d_pos_mass_vecs_raw;
+    cudaMalloc(&d_pos_mass_vecs_raw, sizeof(pos_mass_vector)*h_pos_mass_vecs.size());
     uint32_t* d_offset_raw;
     cudaMalloc(&d_offset_raw, sizeof(uint32_t)*nBodiesToProcess);
     size_t* d_bodies_to_process_raw;
     cudaMalloc(&d_bodies_to_process_raw, sizeof(size_t)*nBodiesToProcess);
-    // real* d_out_phi_raw;
-    // cudaMalloc(&d_out_phi_raw, sizeof(real)*nBodiesToProcess);
-    // cuda_vector* d_out_acc_raw;
-    // cudaMalloc(&d_out_acc_raw, sizeof(cuda_vector)*nBodiesToProcess);
-
-    // real* d_mass_raw;
-    // cudaMalloc(&d_mass_raw, sizeof(real)*nBodiesToProcess);
-    // cuda_vector* d_pos_raw;
-    // cudaMalloc(&d_pos_raw, sizeof(cuda_vector)*nBodiesToProcess);
-    
-    // real h_mass_raw[nBodiesToProcess];
-    // cuda_vector h_pos_raw[nBodiesToProcess];
-
-    // for (size_t i = 0; i < nBodiesToProcess; i++){
-    //     bodyptr current_bptr = bodytab + h_bodies_to_process[i];
-    //     h_mass_raw[]
-    // }
 
 
 
@@ -253,8 +307,10 @@ void cuda_gravsum_dispatch()
                     cudaMemcpyHostToDevice, localCudaStreams[idle_stream]);
     cudaMemcpyAsync(d_bodies_to_process_raw, h_bodies_to_process_raw, sizeof(size_t)*nBodiesToProcess, 
                     cudaMemcpyHostToDevice, localCudaStreams[idle_stream]);
-    cudaMemcpyAsync(d_interact_vecs_raw, h_interact_vecs_raw, sizeof(uint32_t)*h_interact_vecs.size(), 
+    cudaMemcpyAsync(d_pos_mass_vecs_raw, h_pos_mass_vecs_raw, sizeof(pos_mass_vector)*h_pos_mass_vecs.size(),
                     cudaMemcpyHostToDevice, localCudaStreams[idle_stream]);
+    // cudaMemcpyAsync(d_interact_vecs_raw, h_interact_vecs_raw, sizeof(uint32_t)*h_interact_vecs.size(), 
+    //                 cudaMemcpyHostToDevice, localCudaStreams[idle_stream]);
                 
     cudaStreamSynchronize(localCudaStreams[idle_stream]);
     
@@ -270,17 +326,27 @@ void cuda_gravsum_dispatch()
     //     d_out_phi_raw, // phi
     //     d_out_acc_raw, // acc
     //     nBodiesToProcess); // offset for each stream
-    cuda_node_reduction_kernel<<<nBodiesToProcess, blocksize, 0, localCudaStreams[idle_stream]>>>(eps2, 
-        d_interact_vecs_raw, // 1D vector which links all interact lists
+
+    // cuda_node_reduction_kernel<<<nBodiesToProcess, blocksize, 0, localCudaStreams[idle_stream]>>>(eps2, 
+    //     d_interact_vecs_raw, // 1D vector which links all interact lists
+    //     d_offset_raw, // offset for each body in 1D vector
+    //     d_bodies_to_process_raw, 
+    //     device_body_cell_pos_list, // pos
+    //     device_body_cell_mass_list, // mass
+    //     d_out_phi_raw, // phi
+    //     d_out_acc_raw, // acc
+    //     nBodiesToProcess); // offset for each stream
+
+    cuda_node_calc_coalescing_kernel<<<nrGrids, blocksize, 0, localCudaStreams[idle_stream]>>>(eps2, 
+        d_pos_mass_vecs_raw, // 1D vector which links all interact lists
         d_offset_raw, // offset for each body in 1D vector
         d_bodies_to_process_raw, 
-        device_body_cell_pos_list, // pos
-        device_body_cell_mass_list, // mass
         d_out_phi_raw, // phi
         d_out_acc_raw, // acc
         nBodiesToProcess); // offset for each stream
 
-    cudaFreeAsync(d_interact_vecs_raw, localCudaStreams[idle_stream]);
+    //cudaFreeAsync(d_interact_vecs_raw, localCudaStreams[idle_stream]);
+    cudaFreeAsync(d_pos_mass_vecs_raw, localCudaStreams[idle_stream]);
     cudaFreeAsync(d_offset_raw, localCudaStreams[idle_stream]);
     cudaFreeAsync(d_bodies_to_process_raw, localCudaStreams[idle_stream]);
 
@@ -355,18 +421,18 @@ void cuda_gravsum_init() {
     // printf("!!!!!!!!!! Only run on singlethread, struct of array is not thread safe\n");
 
 
-    cudaMalloc(&device_body_cell_mass_list, nbody * 2 * sizeof(real));
-    cudaMalloc(&device_body_cell_pos_list, nbody * 2 * sizeof(cuda_vector));
+    // cudaMalloc(&device_body_cell_mass_list, nbody * 2 * sizeof(real));
+    // cudaMalloc(&device_body_cell_pos_list, nbody * 2 * sizeof(cuda_vector));
 
 
 
-    real body_mass_list[nbody];
+    // real body_mass_list[nbody];
 
-    for (int i = 0; i < nbody; i++) {
-        body_mass_list[i] = Mass(&bodytab[i]);
-    }
+    // for (int i = 0; i < nbody; i++) {
+    //     body_mass_list[i] = Mass(&bodytab[i]);
+    // }
     
-    cudaMemcpy(device_body_cell_mass_list, body_mass_list, nbody * sizeof(real), cudaMemcpyHostToDevice);
+    // cudaMemcpy(device_body_cell_mass_list, body_mass_list, nbody * sizeof(real), cudaMemcpyHostToDevice);
 
 
     cudaMalloc(&d_out_phi_raw, sizeof(real)*nbody);
@@ -376,41 +442,37 @@ void cuda_gravsum_init() {
 
 void cuda_update_body_cell_data() {
 
-    // vector body_cell_pos_list[nbody + ncell];
-    // cuda_vector body_cell_pos_list[nbody + ncell];
-    std::vector<cuda_vector> body_cell_pos_list(nbody+ncell);
+    // std::vector<cuda_vector> body_cell_pos_list(nbody+ncell);
 
-    for (int i = 0; i < nbody; i++) {
-        //SETV(body_cell_pos_list[i], Pos(&bodytab[i]));
-        body_cell_pos_list[i] = VECTOR_TO_CUDA_VECTOR(Pos(&bodytab[i]));
-    }
+    // for (int i = 0; i < nbody; i++) {
+    //     body_cell_pos_list[i] = VECTOR_TO_CUDA_VECTOR(Pos(&bodytab[i]));
+    // }
 
-    for (int i = 0; i < ncell; i++) {
-        //SETV(body_cell_pos_list[i + nbody], Pos(&celltab[i]));
-        body_cell_pos_list[i+nbody] = VECTOR_TO_CUDA_VECTOR(Pos(&celltab[i]));
-    }
+    // for (int i = 0; i < ncell; i++) {
+    //     body_cell_pos_list[i+nbody] = VECTOR_TO_CUDA_VECTOR(Pos(&celltab[i]));
+    // }
 
 
-    cudaMemcpy(device_body_cell_pos_list, body_cell_pos_list.data(), (nbody+ncell) * sizeof(cuda_vector), cudaMemcpyHostToDevice);
+    // cudaMemcpy(device_body_cell_pos_list, body_cell_pos_list.data(), (nbody+ncell) * sizeof(cuda_vector), cudaMemcpyHostToDevice);
 
-    //real cell_mass_list[ncell];
-    std::vector<real> cell_mass_list(ncell);
+    // std::vector<real> cell_mass_list(ncell);
 
-    for (int i = 0; i < ncell; i++) {
-        cell_mass_list[i] = Mass(&celltab[i]);
-    }
-    cudaMemcpy(device_body_cell_mass_list + nbody, cell_mass_list.data(), ncell * sizeof(real), cudaMemcpyHostToDevice);
+    // for (int i = 0; i < ncell; i++) {
+    //     cell_mass_list[i] = Mass(&celltab[i]);
+    // }
+    // cudaMemcpy(device_body_cell_mass_list + nbody, cell_mass_list.data(), ncell * sizeof(real), cudaMemcpyHostToDevice);
 
 
-    h_interact_vecs.clear();
+    //h_interact_vecs.clear();
     h_offset.clear();
     h_bodies_to_process.clear();
+    h_pos_mass_vecs.clear();
 }
 
 
 void cuda_free_all(){
-    cudaFree(device_body_cell_mass_list);
-    cudaFree(device_body_cell_pos_list);
+    //cudaFree(device_body_cell_mass_list);
+    //cudaFree(device_body_cell_pos_list);
     cudaFree(d_out_acc_raw);
     cudaFree(d_out_phi_raw);
     for (int i = 0; i < N_CUDA_STREAMS; i++){
